3aff538538b5f4c12290266c61c94c7066412627
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-06-27 15:47:27 +0300

    ReplyMarkup fixes

--- Type/ForceReply.php
+++ Type/ForceReply.php
@@ -9,12 +9,5 @@ class ForceReply extends ReplyMarkup
      *
      * @var boolean
      */
-    public $force_reply;
-
-    /**
-     * Optional. Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
-     *
-     * @var boolean
-     */
-    public $selective;
+    public $force_reply = true;
 }
--- Type/ReplyKeyboardHide.php
+++ Type/ReplyKeyboardHide.php
@@ -9,12 +9,5 @@ class ReplyKeyboardHide extends ReplyMarkup
      *
      * @var boolean
      */
-    public $hide_keyboard;
-
-    /**
-     * Optional. Use this parameter if you want to hide keyboard for specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
-     *
-     * @var boolean
-     */
-    public $selective;
+    public $hide_keyboard = true;
 }
